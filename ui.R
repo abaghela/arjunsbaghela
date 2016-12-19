d7532f8070fc290d6231a2dc1ff50d79e0bd4289
Arjun Baghela <arjunsbaghela@gmail.com>
2016-12-18 22:30:30 -0800

    hi

--- ui.R
+++ ui.R
@@ -2,12 +2,16 @@ library(shiny)
 
 ui <- fluidPage(
 	includeCSS("www/bootstrap1.css"),
-	h1("arjun baghela"),
+	h1("arjun s baghela"),
 	br(),br(),br(),br(),br(),br(),br(),
 	fluidRow(tabsetPanel(
 	tabPanel("who am i?"),
-	tabPanel("my research")))
-	
+	tabPanel("my research"),
+	tabPanel("useful links",
+					 ("PCA"),br(),a("Youtube Video 1", href="https://www.youtube.com/watch?v=_UVHneBUBW0"),br(), a("Youtube Video 2", href= "https://www.youtube.com/watch?v=tmApJUWWnyI"), br(),
+					 ("K-Means"), br()
+					 
+					 )))
 	
 )
 
