821a2217e3b38315681c58f8d5f0bda26eb09e96
Arjun Baghela <arjunsbaghela@gmail.com>
2016-12-22 22:43:24 -0800

    adding a panel of good to read.

--- ui.R
+++ ui.R
@@ -11,7 +11,13 @@ ui <- fluidPage(
 					 ("PCA"),br(),a("Youtube Video 1", href="https://www.youtube.com/watch?v=_UVHneBUBW0"),br(), a("Youtube Video 2", href= "https://www.youtube.com/watch?v=tmApJUWWnyI"), br(),
 					 ("K-Means"), br()
 					 
-					 )))
+					 ),
+	tabPanel("books to read",
+					 "Good to Great", br(),
+					 "The Big Short", br(),
+					 "Algorithms to Live By: The Computer Science of Human Decisions", br()
+					 )
+	))
 	
 )
 
